dd0c426cc8f8b2c47a694b08400eada0eeb0e992
Vedavyaas M R <itsvyaas32@gmail.com>
2025-08-08 20:49:09 +0530

    Implemented Whatsapp services

--- Backend-Summarizer/src/main/java/com/Summarizer/Controller/TwilioController.java
+++ Backend-Summarizer/src/main/java/com/Summarizer/Controller/TwilioController.java
@@ -1,3 +1,13 @@
+/*      Run your Spring Boot app locally on port 8001.
+        Download and install ngrok from https://ngrok.com/download or use this in terminal brew install ngrok/ngrok/ngrok.
+        Open your terminal and run:
+        ##ngrok http 8001##
+        Copy the https URL that ngrok gives you (like https://abc123.ngrok.io).
+        Go to your Twilio dashboard and set the webhook URL to:
+        https://80b337899e4e.ngrok-free.app/twilio/webhook
+        Now Twilio will send WhatsApp messages to your local app through ngrok!
+        Keep ngrok running while you test.    */
+
 package com.Summarizer.Controller;
 
 import com.Summarizer.Config.TwilioConfig;
